f964db9aeb1beba4aa3a709c4ec50c10043640b3
Piotr Jankowski <96870646+piotrek-jankowski@users.noreply.github.com>
2025-12-06 00:31:32 +0100

    Add general configuration preferences for Firefox

--- ansible/.files/firefox/local-settings.js
+++ ansible/.files/firefox/local-settings.js
@@ -1,3 +1,4 @@
 // /usr/lib/firefox/defaults/pref/local-settings.js
+// -rw-r--r-- 1 root root
 pref("general.config.filename", "mozilla.cfg");
 pref("general.config.obscure_value", 0); // 0 = no masking
